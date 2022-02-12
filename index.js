13cda18be4dc6221f2ca36bc9e049be384c14e84
christopher4lis <christopher4lis@gmail.com>
2022-02-12 01:14:32 -0500

    Project Setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
