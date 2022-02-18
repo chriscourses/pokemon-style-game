ffc51bfb133e76e32ad1402b61a85f1df713904a
christopher4lis <christopher4lis@gmail.com>
2022-02-18 16:08:09 -0500

    Import and Render Map

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ canvas.height = 576
 
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './img/Pellet Town.png'
+
+image.onload = () => {
+  c.drawImage(image, -750, -550)
+}
