d98e6842fb406ccd8a9f18810dab0d6e00bf0dd8
christopher4lis <christopher4lis@gmail.com>
2022-02-18 16:09:31 -0500

    Player Creation

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const image = new Image()
 image.src = './img/Pellet Town.png'
 
+const playerImage = new Image()
+playerImage.src = './img/playerDown.png'
+
 image.onload = () => {
-  c.drawImage(image, -750, -550)
+  c.drawImage(image, -785, -650)
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 4 / 2,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+  )
 }
