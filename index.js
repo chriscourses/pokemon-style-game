6926de03bf4473fe1a2283e19a267aec1a6c7730
christopher4lis <christopher4lis@gmail.com>
2022-02-18 16:13:34 -0500

    Move Player Through Map on Keydown

--- index.js
+++ index.js
@@ -13,8 +13,43 @@ image.src = './img/Pellet Town.png'
 const playerImage = new Image()
 playerImage.src = './img/playerDown.png'
 
-image.onload = () => {
-  c.drawImage(image, -785, -650)
+class Sprite {
+  constructor({ position, velocity, image }) {
+    this.position = position
+    this.image = image
+  }
+
+  draw() {
+    c.drawImage(this.image, this.position.x, this.position.y)
+  }
+}
+
+const background = new Sprite({
+  position: {
+    x: -785,
+    y: -650
+  },
+  image: image
+})
+
+const keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  background.draw()
   c.drawImage(
     playerImage,
     0,
@@ -26,4 +61,51 @@ image.onload = () => {
     playerImage.width / 4,
     playerImage.height
   )
+
+  if (keys.w.pressed && lastKey === 'w') background.position.y += 3
+  else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
+  else if (keys.s.pressed && lastKey === 's') background.position.y -= 3
+  else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3
 }
+animate()
+
+let lastKey = ''
+window.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'w':
+      keys.w.pressed = true
+      lastKey = 'w'
+      break
+    case 'a':
+      keys.a.pressed = true
+      lastKey = 'a'
+      break
+
+    case 's':
+      keys.s.pressed = true
+      lastKey = 's'
+      break
+
+    case 'd':
+      keys.d.pressed = true
+      lastKey = 'd'
+      break
+  }
+})
+
+window.addEventListener('keyup', (e) => {
+  switch (e.key) {
+    case 'w':
+      keys.w.pressed = false
+      break
+    case 'a':
+      keys.a.pressed = false
+      break
+    case 's':
+      keys.s.pressed = false
+      break
+    case 'd':
+      keys.d.pressed = false
+      break
+  }
+})
