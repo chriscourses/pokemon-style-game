fddf8c836c549dbad39682a92c0fac771d04265d
christopher4lis <christopher4lis@gmail.com>
2022-02-24 14:03:10 -0500

    Player Movement Animation

--- classes.js
+++ classes.js
@@ -1,19 +1,21 @@
 class Sprite {
-  constructor({ position, velocity, image, frames = { max: 1 } }) {
+  constructor({ position, velocity, image, frames = { max: 1 }, sprites }) {
     this.position = position
     this.image = image
-    this.frames = frames
+    this.frames = { ...frames, val: 0, elapsed: 0 }
 
     this.image.onload = () => {
       this.width = this.image.width / this.frames.max
       this.height = this.image.height
     }
+    this.moving = false
+    this.sprites = sprites
   }
 
   draw() {
     c.drawImage(
       this.image,
-      0,
+      this.frames.val * this.width,
       0,
       this.image.width / this.frames.max,
       this.image.height,
@@ -22,6 +24,17 @@ class Sprite {
       this.image.width / this.frames.max,
       this.image.height
     )
+
+    if (!this.moving) return
+
+    if (this.frames.max > 1) {
+      this.frames.elapsed++
+    }
+
+    if (this.frames.elapsed % 10 === 0) {
+      if (this.frames.val < this.frames.max - 1) this.frames.val++
+      else this.frames.val = 0
+    }
   }
 }
 
--- index.js
+++ index.js
@@ -35,19 +35,35 @@ image.src = './img/Pellet Town.png'
 const foregroundImage = new Image()
 foregroundImage.src = './img/foregroundObjects.png'
 
-const playerImage = new Image()
-playerImage.src = './img/playerDown.png'
+const playerDownImage = new Image()
+playerDownImage.src = './img/playerDown.png'
+
+const playerUpImage = new Image()
+playerUpImage.src = './img/playerUp.png'
+
+const playerLeftImage = new Image()
+playerLeftImage.src = './img/playerLeft.png'
+
+const playerRightImage = new Image()
+playerRightImage.src = './img/playerRight.png'
 
 const player = new Sprite({
   position: {
     x: canvas.width / 2 - 192 / 4 / 2,
     y: canvas.height / 2 - 68 / 2
   },
-  image: playerImage,
+  image: playerDownImage,
   frames: {
     max: 4
+  },
+  sprites: {
+    up: playerUpImage,
+    left: playerLeftImage,
+    right: playerRightImage,
+    down: playerDownImage
   }
 })
+console.log(player)
 
 const background = new Sprite({
   position: {
@@ -100,7 +116,11 @@ function animate() {
   foreground.draw()
 
   let moving = true
+  player.moving = false
   if (keys.w.pressed && lastKey === 'w') {
+    player.moving = true
+    player.image = player.sprites.up
+
     for (let i = 0; i < boundaries.length; i++) {
       const boundary = boundaries[i]
       if (
@@ -125,6 +145,9 @@ function animate() {
         movable.position.y += 3
       })
   } else if (keys.a.pressed && lastKey === 'a') {
+    player.moving = true
+    player.image = player.sprites.left
+
     for (let i = 0; i < boundaries.length; i++) {
       const boundary = boundaries[i]
       if (
@@ -149,6 +172,9 @@ function animate() {
         movable.position.x += 3
       })
   } else if (keys.s.pressed && lastKey === 's') {
+    player.moving = true
+    player.image = player.sprites.down
+
     for (let i = 0; i < boundaries.length; i++) {
       const boundary = boundaries[i]
       if (
@@ -173,6 +199,9 @@ function animate() {
         movable.position.y -= 3
       })
   } else if (keys.d.pressed && lastKey === 'd') {
+    player.moving = true
+    player.image = player.sprites.right
+
     for (let i = 0; i < boundaries.length; i++) {
       const boundary = boundaries[i]
       if (
