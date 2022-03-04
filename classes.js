2386aa91738ea5911e3c698eb81715d164fdad6e
christopher4lis <christopher4lis@gmail.com>
2022-03-04 17:38:55 -0500

    Add Battle Sprites

--- classes.js
+++ classes.js
@@ -1,5 +1,12 @@
 class Sprite {
-  constructor({ position, velocity, image, frames = { max: 1 }, sprites }) {
+  constructor({
+    position,
+    velocity,
+    image,
+    frames = { max: 1, hold: 10 },
+    sprites,
+    animate = false
+  }) {
     this.position = position
     this.image = image
     this.frames = { ...frames, val: 0, elapsed: 0 }
@@ -8,7 +15,7 @@ class Sprite {
       this.width = this.image.width / this.frames.max
       this.height = this.image.height
     }
-    this.moving = false
+    this.animate = animate
     this.sprites = sprites
   }
 
@@ -25,13 +32,13 @@ class Sprite {
       this.image.height
     )
 
-    if (!this.moving) return
+    if (!this.animate) return
 
     if (this.frames.max > 1) {
       this.frames.elapsed++
     }
 
-    if (this.frames.elapsed % 10 === 0) {
+    if (this.frames.elapsed % this.frames.hold === 0) {
       if (this.frames.val < this.frames.max - 1) this.frames.val++
       else this.frames.val = 0
     }
--- index.js
+++ index.js
@@ -77,7 +77,8 @@ const player = new Sprite({
   },
   image: playerDownImage,
   frames: {
-    max: 4
+    max: 4,
+    hold: 10
   },
   sprites: {
     up: playerUpImage,
@@ -147,7 +148,7 @@ function animate() {
   foreground.draw()
 
   let moving = true
-  player.moving = false
+  player.animate = false
 
   console.log(animationId)
   if (battle.initiated) return
@@ -207,7 +208,7 @@ function animate() {
   }
 
   if (keys.w.pressed && lastKey === 'w') {
-    player.moving = true
+    player.animate = true
     player.image = player.sprites.up
 
     for (let i = 0; i < boundaries.length; i++) {
@@ -234,7 +235,7 @@ function animate() {
         movable.position.y += 3
       })
   } else if (keys.a.pressed && lastKey === 'a') {
-    player.moving = true
+    player.animate = true
     player.image = player.sprites.left
 
     for (let i = 0; i < boundaries.length; i++) {
@@ -261,7 +262,7 @@ function animate() {
         movable.position.x += 3
       })
   } else if (keys.s.pressed && lastKey === 's') {
-    player.moving = true
+    player.animate = true
     player.image = player.sprites.down
 
     for (let i = 0; i < boundaries.length; i++) {
@@ -288,7 +289,7 @@ function animate() {
         movable.position.y -= 3
       })
   } else if (keys.d.pressed && lastKey === 'd') {
-    player.moving = true
+    player.animate = true
     player.image = player.sprites.right
 
     for (let i = 0; i < boundaries.length; i++) {
@@ -327,11 +328,45 @@ const battleBackground = new Sprite({
   },
   image: battleBackgroundImage
 })
+
+const draggleImage = new Image()
+draggleImage.src = './img/draggleSprite.png'
+const draggle = new Sprite({
+  position: {
+    x: 800,
+    y: 100
+  },
+  image: draggleImage,
+  frames: {
+    max: 4,
+    hold: 30
+  },
+  animate: true
+})
+
+const embyImage = new Image()
+embyImage.src = './img/embySprite.png'
+const emby = new Sprite({
+  position: {
+    x: 280,
+    y: 325
+  },
+  image: embyImage,
+  frames: {
+    max: 4,
+    hold: 30
+  },
+  animate: true
+})
+
 function animateBattle() {
   window.requestAnimationFrame(animateBattle)
   battleBackground.draw()
+  draggle.draw()
+  emby.draw()
 }
 
+// animate()
 animateBattle()
 
 let lastKey = ''
